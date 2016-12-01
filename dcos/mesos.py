d8d4bb6b4bd5e0868356d1b47c24bd178b2bf940
Steven Locke <slocke@mesosphere.io>
2016-11-30 16:42:34 -0800

    Added a test for get_chunked_msg

--- dcos/mesos.py
+++ dcos/mesos.py
@@ -11,7 +11,6 @@ import mesos.v1.mesos_pb2 as pbm
 from functools import partial
 
 from google.protobuf.json_format import MessageToJson, Parse
-# from google.protobuf import ParseFromString
 
 import threading
 
@@ -444,6 +443,7 @@ class Master(object):
         return tasks
 
     # TODO: Currently waiting on this mapping in state.json to be present.
+    # This mapping is only present in DC/OS 1.9+
     def get_container_id(self, task_id):
         """Returns the container ID for a task ID matching `task_id`
 
@@ -1087,6 +1087,41 @@ class TaskIO(object):
         os.read(fileno, 2)
         return chunk
 
+
+    def test_get_chunked_msg(self):
+        """Tests getting a chunked message
+        """
+        
+        msg = b'Test Message.'
+        filename = 'testfile.txt'
+        chunk = '%X\r\n%s\r\n' % (len(msg), msg.decode('utf-8'))
+
+        try:
+            try:
+                with open(filename, 'w') as file:
+                    file.write(chunk)
+            except Exception as exception:
+                raise DCOSException(
+                    "Error writing to {filename} in test_get_chunked_msg: \
+                    {error}".format(filename=filename, error=exception))
+
+            try:
+                with open(filename, 'r') as file:
+                    chunked_msg = self.get_chunked_msg(file.fileno())
+            except Exception as exception:
+                raise DCOSException(
+                    "Error reading from {filename} in test_get_chunked_msg: \
+                    {error}".format(filename=filename, error=exception))
+
+            assert chunked_msg == msg
+        finally:
+            try:
+                import os
+                os.remove(filename)
+            except OSError:
+                pass
+
+
     def _launch_container_session(self):
         """Sends a request to the Mesos Agent API to attach the
         STDOUT stream of an already running container.
@@ -1131,7 +1166,7 @@ class TaskIO(object):
                             self.output_queue.put(r.data)
 
             except:
-                raise DCOSException('Invalid message type passed to _launch_container_session')
+                raise DCOSException('Invalid message type passed to _attach_output_stream')
 
         self.output_queue.join()
         self.exit_event.wait()
